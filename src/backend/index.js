27ee691d4e2e28469a16a0c6cb314fe1f038bc01
Degtyarev Alexander <adegtyarev.ap@gmail.com>
2023-09-04 12:16:48 +0300

    Добавил контроллер для ролей

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const userRouter = require('./routes/user.routes')
+const roleRouter = require('./routes/role.routes')
 
 const PORT = process.env.PORT || 8081
 
@@ -7,5 +8,6 @@ const app = express()
 
 app.use(express.json())
 app.use('/api', userRouter)
+app.use('/api', roleRouter)
 
 app.listen(PORT, () => console.log(`server started on post ${PORT}`))
