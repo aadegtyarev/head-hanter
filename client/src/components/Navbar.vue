04b6586396abe53aa35e324d93608d6bda4154d0
wb-adegtyarev <a.degtyarev@wirenboard.com>
2023-09-16 21:41:58 +0400

    Добавил курсор

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="navbar">
-        <div @click="$router.push('/')">Head Hanter App</div>
+        <div
+            class="logo"
+            @click="$router.push('/')"
+        >Head Hanter App</div>
         <div class="navbar__btns">
             <my-button @click="$router.push('/jobs')">Вакансии</my-button>
         </div>
@@ -20,12 +23,18 @@
     align-items: center;
     padding: 0 15px;
 }
-.navbar__btns{
+
+.navbar__btns {
     margin-left: auto;
-    margin: 5px 5px 5px auto;   
+    margin: 5px 5px 5px auto;
 }
-.second__btn{
-    margin-left: 20px;    
+
+.second__btn {
+    margin-left: 20px;
+}
+
+.logo {
+    cursor: pointer;
 }
 </style>
         
