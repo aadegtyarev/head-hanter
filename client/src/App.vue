d973a91bd4def8c7e44446996e366d1763e4950a
wb-adegtyarev <a.degtyarev@wirenboard.com>
2023-09-17 13:00:05 +0400

    Стили

--- client/src/App.vue
+++ client/src/App.vue
@@ -18,14 +18,4 @@ export default {
 
 </script>
 
-<style>
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-
-.app {
-    padding: 20px;
-}
-</style>
+<style></style>
--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -5,6 +5,7 @@
             @click="$router.push('/')"
         >Head Hanter App</div>
         <div class="navbar-btns">
+            <my-button @click="$router.push('/')">Главная</my-button>
             <my-button @click="$router.push('/jobs')">Вакансии</my-button>
         </div>
     </div>
