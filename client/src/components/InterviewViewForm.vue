86b9d306f427f56ddc82e6602c31f453cfa34d14
wb-adegtyarev <a.degtyarev@wirenboard.com>
2023-09-19 17:43:49 +0400

    Добавил вывод требований к вакансии

--- client/src/components/InterviewViewForm.vue
+++ client/src/components/InterviewViewForm.vue
@@ -2,15 +2,24 @@
     <div>
         <div>
             <div>
-                <h1> Интервью на вакансию «{{ interview.job_title }}»</h1>
-                <h2>{{ interview.applicant_name }} [<a :href="`${response.resume_url}`">резюме</a>]</h2>
+                <h1> Интервью</h1>
+                <h2>Вакансия «{{ interview.job_title }}»</h2>
                 <div>
-                    <a :href="`mailo:${response.email}`">{{ response.email }}</a>
+                    <p><strong>Зарплата от </strong>{{ job.salary_from }} до {{ job.salary_to }} руб</p>
+                    <p><strong>Образование: </strong>{{ job.education }}</p>
+                    <p><strong>Требуемый опыт: </strong>{{ job.experience }}</p>
+                    <p><strong>Нужные навыки: </strong>{{ job.skills }}</p>
+                    <p><strong>Ссылка на тестовое задание: </strong>{{ job.test_doc }}</p>
+                    <p><strong>Описание: </strong>{{ job.detail }}</p>
+                </div>
+                <h2> {{ interview.applicant_name }} [<a :href="`${response.resume_url}`">резюме</a>]</h2>
+                <div>
+                    <p><strong>Зарплата: </strong>{{ response.salary_desired }} руб</p>
                     <p><strong>Образование: </strong>{{ response.education }}</p>
                     <p><strong>Опыт работы: </strong>{{ response.experience }}</p>
-                    <p><strong>Хочет зарплату: </strong>{{ response.salary_desired }} руб</p>
                     <p><strong>Результат теста: </strong>{{ response.questionnaire_result }}</p>
                     <p><strong>Заметки: </strong>{{ response.description }}</p>
+                    <a :href="`mailo:${response.email}`">{{ response.email }}</a>
                 </div>
                 <h2>Интервью</h2>
                 <p><strong>Дата и время: </strong>{{ interview.date_human }}</p>
@@ -32,6 +41,7 @@
 
 <script>
 import useGetResponse from "@/hooks/useGetResponse"
+import useGetJob from "@/hooks/useGetJob"
 
 export default {
     methods: {
@@ -47,9 +57,11 @@ export default {
     },
     setup(props) {
         const { response } = useGetResponse(props.interview.response_id)
+        const { job } = useGetJob(props.interview.job_id)
 
         return {
-            response
+            response,
+            job
         }
     }
 }
