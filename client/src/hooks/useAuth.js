c8834a7bc34b6d2f04a858de75ddf1dae85a30f0
wb-adegtyarev <a.degtyarev@wirenboard.com>
2023-09-26 15:32:09 +0400

    Оптимизация

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,11 +1,47 @@
 import { onMounted, ref } from "vue";
 import axios from "axios";
 import useToken from "./useToken";
+import store from "@/store";
+import router from "@/router/router";
 
 export default function useAuth() {
     const { createToken, deleteToken, checkToken } = useToken()
 
-    const login = (auth) => {
+    const login = async (auth) => {
+        const [response] = await Promise.all([
+            checkUser(auth)
+        ]);
+
+        if (response.isValid) {
+            store.state.auth.isAuth = true
+            router.push('/')
+
+            const [token] = await Promise.all([
+                createToken(response.user_id)
+            ]);
+            // console.log(token)
+
+            $cookies.set("head-hunter", {
+                "id": response.user_id,
+                "token": token
+            }, "7d")
+        }
+    };
+
+    const logout = async () => {
+        try {
+            if ($cookies.get("head-hunter")) {
+                const user_id = $cookies.get("head-hunter").id
+                deleteToken(user_id)
+                $cookies.remove("head-hunter")
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+        }
+    };
+
+    const checkUser = (auth) => {
         return new Promise((resolve, reject) => {
             axios.get("/user-valid-password",
                 {
@@ -20,18 +56,6 @@ export default function useAuth() {
                 reject(error);
             })
         })
-    };
-    const logout = async () => {
-        try {
-            if ($cookies.get("head-hunter")) {
-                const user_id = $cookies.get("head-hunter").id
-                deleteToken(user_id)
-                $cookies.remove("head-hunter")
-            }
-        } catch (error) {
-            console.log(error);
-        } finally {
-        }
     };
 
     return {
--- client/src/pages/Login.vue
+++ client/src/pages/Login.vue
@@ -14,33 +14,15 @@ export default {
         LoginForm,
     },
     methods: {
-        async log_in(auth) {
-            const [response] = await Promise.all([
-                this.login(auth)
-            ]);
-
-            if (response.isValid) {
-                this.$store.state.auth.isAuth = true
-                this.$router.push('/')
-
-                const [token] = await Promise.all([
-                    this.createToken(response.user_id)
-                ]);
-                console.log(token)
-
-                $cookies.set("head-hunter", {
-                    "id": response.user_id,
-                    "token": token
-                }, "7d")
-            }
+        log_in(auth) {
+            this.login(auth)
         },
     },
     setup(props) {
-        const { login, createToken } = useAuth()
+        const { login } = useAuth()
 
         return {
-            login,
-            createToken
+            login
         }
     }
 
